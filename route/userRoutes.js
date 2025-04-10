cdb5805a56199ad695a57f776bcd2638561fea22
Abhinav-FP <a.mathur@futureprofilez.com>
2025-04-10 18:25:16 +0530

    added updated profile and reste password apis

--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,11 +1,12 @@
-const { signup, login, userId } = require("../controller/authController");
+const { signup, login, updateProfile, resetPassword, GetUser } = require("../controller/authController");
 const { verifyToken } = require("../middleware/tokenVerify");
 
 const router = require("express").Router();
 
 router.post("/user/register", signup);
 router.post("/user/login", login);
-router.get("/user/profile", verifyToken ,userId );
-
+router.get("/user/profile", verifyToken ,GetUser);
+router.get("/user/update-profile", verifyToken ,updateProfile );
+router.get("/user/reset-password", verifyToken ,resetPassword );
 
 module.exports = router;
