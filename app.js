24029d276e11d0a8f4c9e133f99609c0877e79c2
Abhinav-FP <a.mathur@futureprofilez.com>
2025-04-10 16:14:39 +0530

    dg

---
+++ app.js
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+require("./dbconfigration");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: "*", // Allowed origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: '*', // Allow all headers
+  credentials: true,
+  optionsSuccessStatus: 200, // for legacy browsers
+}
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: '2000mb' }));
+app.use(express.urlencoded({ extended: true, limit: "2000mb" }));
+
+const PORT = process.env.REACT_APP_SERVER_DOMAIN || 5000;
+
+
+app.use("/api", require("./route/userRoutes"));
+app.use("/api", require("./route/messageRoutes"));
+app.use("/api", require("./route/wishlistRoutes"));
+app.use("/api", require("./route/lessonRoutes"));
+app.use("/api", require("./route/homeRoutes"));
+app.use("/api", require("./route/paymentRoutes"));
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: 'Hello World',
+    status: 200,
+  });
+});
+
+const server = app.listen(PORT, () => console.log("Server is running at port : " + PORT));
+server.timeout = 360000; // 6 minutes
