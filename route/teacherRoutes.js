dcb12f62ec3e8fc8d4445d4aa017b50cfa38276b
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-05-01 10:26:41 +0530

    thre page contact page

--- route/teacherRoutes.js
+++ route/teacherRoutes.js
@@ -8,7 +8,7 @@ router.put("/teacher/availability/update/:id" , verifyToken , UpdateAvailability
 
 router.delete("/teacher/availability/delete/:id", verifyToken, RemoveAvailability);
 
-router.get("/teacher/availability/get/:id", verifyToken, GetAvailability);
+router.get("/teacher/availability/get", verifyToken, GetAvailability);
 
 router.get("/teacher/lesson/get", verifyToken, GetLessons);
 
