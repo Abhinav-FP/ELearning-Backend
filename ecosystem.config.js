9a2eeb69c846ee885b87b0d0e4e609c14aba2611
Abhinav-FP <a.mathur@futureprofilez.com>
2025-05-13 14:52:58 +0530

    Updated ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         watch: true,
         name: 'ELearning-Backend',
         script: './app.js',
-        cwd: '/home/ubuntu/ELearning-Backend',
+        cwd: '/home/ubuntu/ELearning-Backend/app.js',
         env: {
           NODE_ENV: 'production',
         },
