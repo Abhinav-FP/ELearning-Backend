d97e6bb77162a8f17303a1010b5f723b631ac1ca
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-05-29 10:47:00 +0530

    manage

--- EmailTemplate/Forgetpassword.js
+++ EmailTemplate/Forgetpassword.js
@@ -17,7 +17,7 @@ module.exports = (resetLink, userName) => {
         <tr>
             <td style="padding: 0rem 1rem 1rem;border-bottom: 1px solid rgba(0,0,0,.1)">
                 <p style="font-size: 1.5rem; font-weight: bold; line-height: 1.9rem; text-align: center;color: #CC2828;margin: 0 0 .6rem;">Forgot your password ?</p>  
-                <p style="font-size: 1.1rem; font-weight: bold; line-height: 1.6rem; text-align: center;color: #333333;margin: 0.8rem 0 .7rem;">Hi ${userName},</p>
+                <p style="font-size: 1.1rem; font-weight: bold; line-height: 1.6rem; text-align: center;color: #333333;margin: 0.8rem 0 .7rem; text-transform: capitalize;">Hi ${userName},</p>
                 <p style="font-size: 1rem; font-weight: 400; line-height: 1.5rem; text-align: center;color: #333333;margin: 0 0 1.1rem;">We received a request to reset your password. Click the button below to set a new one:
                 </p> 
                 <p style="margin: 0 0 1.3rem;text-align: center;"><a href=${resetLink} style="background:#CC2828;color:#fff;border-radius: 7px;font-size: 1.1rem;text-decoration: none;display: inline-block;padding: .8rem 1.5rem;">Reset Password</a></p> 
