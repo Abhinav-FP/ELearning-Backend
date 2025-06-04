9e25c486e75347cb77af26350c69fd6975a03b54
Abhinav-FP <a.mathur@futureprofilez.com>
2025-06-04 17:35:33 +0530

    Cron job

---
+++ model/SpecialSlot.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const SpecialSlotSchema = mongoose.Schema({
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Student ID is required."],
+    },
+    teacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Teacher ID is required."],
+    },
+    lesson: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+        required: [true, "Lesson id is required"],
+    },
+    paymentStatus: {
+        type: String,
+        enum: ["paid", "pending"],
+        default: "pending",
+    },
+    startDateTime: {
+      type: Date,
+      required: true,
+    },
+    endDateTime: {
+      type: Date,
+      required: true,
+    },
+});
+
+const SpecialSlots = mongoose.model("SpecialSlot", SpecialSlotSchema);
+module.exports = SpecialSlots;
